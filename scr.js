4ed10a8cc0f0b88181eb2f208d88229d0649c3ee
Adam Simonini <adam.simonini@gmail.com>
2017-07-07 17:43:12 -0400

    Added prepend of Divs
    Now displays the amount of appearences all characters that appear more
    than once have within the string

--- scr.js
+++ scr.js
@@ -7,29 +7,50 @@ $(document).ready(function(){
   //  return substrings.length -1;
   // }
 
-  function duplicateChecker(){
-    var duplicates = 0;
-    for(i=0;i<str.length;i++){
-      var count = str.match(new RegExp(str[i], "g") || []).length;
-      // var count = countInstances(str, str[i]);
-      if (count > 1){
-        removePermutations(count);
-        console.log("We have " + count + " duplicates of " + str[i]);
-      }else{
-        console.log("No duplicates, yet...");
-      }
-    }
-    console.log(duplicates);
+  function createDiv(char, num){
+    var master = document.getElementById("masterDiv");
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = '<div id="Character-' + char + '" class=""><p>There are ' + num + ' instances of the character ' +  char.toUpperCase() + '</div>'
+    master.prepend(newDiv);
   }
 
-  function factoralizeString(){
-    var total = 1;
-    length = str.length;
+  function duplicateChecker(){
+    var deduction = 0;
     for(i=0;i<str.length;i++){
-      total *= length;
-      length -= 1;
+      var count = str.match(new RegExp(str[i], "g")).length;
+      // var count = countInstances(str, str[i]);
+      if (count > 1){
+        console.log("We have " + count + " duplicates of " + str[i]);
+        deduction += factorializeNumber(count);
+        createDiv(str[i], count);
+      }else{
+        console.log(str[i] + " has no duplicates");
+      }
     }
-    return total;
+    // console.log(deduction);
+  }
+
+  function factorializeNumber(num){
+
+    var numTotal = 1;
+    var length = num;
+    var factor = num
+    // for(i=0;i < length;i++){
+    //   numTotal *= factor;
+    //   factor--;
+    // }
+    return numTotal;
+  }
+
+
+  function factoralizeString(){
+    var stringTotal = 1;
+    var length = str.length;
+    for(i=0;i<str.length;i++){
+      stringTotal *= length;
+      length--;
+    }
+    return stringTotal;
   }
 
   $("#permuteBtn").on("click", function(){
